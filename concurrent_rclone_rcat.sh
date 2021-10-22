bff455d827263ede5a837e2a1b614c7eea3265c5
Kimmo.Mattila <kimmo.mattila@csc.fi>
2021-10-22 11:47:04 +0300

    md5 sums in allas-dir-to-bucket

--- concurrent_rclone_rcat.sh
+++ concurrent_rclone_rcat.sh
@@ -43,6 +43,58 @@ upload_segment() {
     echo "   Segment $1/$3 ready"
 };
 
+
+
+upload_segment_md5() {
+    echo "   Starting to upload a segment $1/$3"
+    # Using 5GiB segments as specified by Openstack Swift protocol
+    # Using Openstack tools naming scheme for segments and containers
+    # Using 1MiB block size for dd
+
+    # $1 is the file to be uploaded, # $2 is the destination container,
+    # $3 is the index, $4 the timestamp, $5 the filesize
+    #echo "dd status=none if=$1 bs=1M count=5120 skip=$(echo "5120 * $3" | bc) "
+    #echo "rclone --swift-no-chunk rcat $RCLONE_DESTINATION:$2_segments/$1/$4/$5/$(printf "%08d" $3)"
+    local skipvalue=$(echo "5120 * $3" | bc)
+    local s_kesto=0
+    local s_alku=$(date +%s)
+    local do_upload=1
+    local s_loppu=$(date +%s)
+    local ite=0
+    while [[ $do_upload -eq 1 ]]; do
+      s_alku=$(date +%s)
+      dd status=none if=$1 bs=1M count=5120 skip=$(echo "5120 * $3" | bc) \
+        | rclone --swift-no-chunk rcat $RCLONE_DESTINATION:$2_segments/$1/$4/$5/$(printf "%08d" $3)
+    
+      s_loppu=$(date +%s)
+      (( s_kesto = s_loppu - s_alku ))
+      echo "   Segment upload $1/$3 ready in $s_kesto seconds"
+      echo "     Calculating md5 sums for $1/$3"
+      s_alku=$(date +%s)
+      local md5string=$(dd status=none if=$1 bs=1M count=5120 skip=$skipvalue | md5sum - | awk '{print $1}')
+      local md5allas=$(rclone md5sum $RCLONE_DESTINATION:$2_segments/$1/$4/$5/$(printf "%08d" $3) | awk '{print $1}')
+      s_loppu=$(date +%s)
+      (( s_kesto = s_loppu - s_alku ))
+      if [[ "$md5string" == "$md5allas" ]]; then
+         echo "     Checksums match for $1/$3. Checking took $s_kesto seconds."
+         echo "   Segment $1/$3 ready."
+         do_upload=0	 
+      else
+         echo "ERROR: for $1/$3."
+         echo "Local and Allas checksums don't match"
+         rclone delete  $RCLONE_DESTINATION:$2_segments/$1/$4/$5/$(printf "%08d" $3)
+         ((ite = ite + 1 ))
+         if [[ $ite -lt 3 ]]; then
+            echo "Retrying faled segment upload."
+         else
+            echo "Too many failed upoload attempts."
+            do_upload=0
+         fi
+      fi
+   done
+};
+
+
 push_manifest() {
     echo "   Push Openstack Swift DLO manifest"    
     curl -s -X PUT "$SWIFT_URL/${2}/${1}" \
@@ -54,6 +106,7 @@ push_manifest() {
 
 timestamp=$(date +%s);
 filesize=$(stat --printf="%s" $1);
+check_md5=$3
 
 # skip files < 5GiB – we have tools for those
 if [[ $filesize -le 5368709120 ]]; then
@@ -78,7 +131,11 @@ for ((i = 0; i * 5368709120 < $filesize; i++)); do
     while [[ $(jobs -p |wc -l) -ge $MAX_PROCESSES ]]; do
         sleep 5
     done
-    upload_segment $1 $2 $i $timestamp $filesize &  # fork a new process for each segment
+    if [[ $check_md5 -eq 1 ]]; then
+       upload_segment_md5 $1 $2 $i $timestamp $filesize &  # fork a new process for each segment
+    else 
+       upload_segment $1 $2 $i $timestamp $filesize &
+    fi
 done
 
 # Wait for all parallel jobs to finish – this method of waiting allows for using ctrl-z as well, if need be
